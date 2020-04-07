2efd2f4391e1dd42211732a2dc41897c7bb872c3
YaCpotato <yasshisshy@gmail.com>
2020-04-07 16:08:44 +0900

    API記述完了

---
+++ app/Http/Controllers/QuestionController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class QuestionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $questions = Question::all();
+        return view('question/index', compact('questions'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('question/create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $question = new question;
+        $question->content = $request->name;
+        $question->answer = $request->name;
+        $question->comments = $request->name;
+        $question->user_id = \Auth::user()->id;
+        $question->save();
+        return redirect('question/'.$question->id);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $question = Question::find($id);
+        return view('question/detail', compact('question'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $question = Question::find($id);
+        return view('question/edit', compact('question'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $question = Question::findOrFail($id);
+        $question->content = $request->content;
+        $question->answer = $request->answer;
+        $question->comments = $request->comments;
+        $question->save();
+        return view('question/detail',compact('question'));
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $question = Question::findOrFail($id);
+        $question->delete();
+        
+        $questions = Question::all();
+        return view('question/index', compact('questions'));
+    }
+}
