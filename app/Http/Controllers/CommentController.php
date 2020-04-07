4eebe2ffbe60fdefd71950226c5907082fcaa97c
YaCpotato <yasshisshy@gmail.com>
2020-04-07 17:01:17 +0900

    comment API 微修正

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Question;
 
 class CommentController extends Controller
 {
@@ -10,12 +11,12 @@ class CommentController extends Controller
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
-     * @param int id
+     * @param int questionId 
      */
-    public function index($id)
+    public function index($questionId)
     {
-        $comments = Comment::query()->where('question_id',$id)->get();        
-        return view('comment/index', compact('comments'));
+        $comments = Comment::query()->where('question_id',$questionId)->get();        
+        return $comments;
     }
 
     /**
@@ -32,19 +33,20 @@ class CommentController extends Controller
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  int  $id question_id
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request, $id)
+    public function store(Request $request, $questionId)
     {
         $comment = new comment;
         $comment->comment = $request->comment;
         $comment->user_id = \Auth::user()->id;
-        $comment->question_id = Question::findOrFail($id)->id;
+        $comment->question_id = Question::findOrFail($questionId)->id;
         $comment->save();
-        
-        $comments = Comment::all();
-        return view('comment/index', compact('comments'));
+
+        $comments = Comment::query()->where('question_id',$questionId)->get();
+        $question = Question::find($questionId);
+        return view('question/detail', compact('question','comments'));
     }
 
     /**
@@ -75,17 +77,19 @@ class CommentController extends Controller
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  int $id
+     * @param  int  $questionId
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, $id, $questionId)
     {
         $comment = Comment::findOrFail($id);
         $comment->comment = $request->comment;
         $comment->save();
 
-        $comments = Comment::all();
-        return view('comment/index', compact('comments'));
+        $question = Question::find($questionId);
+        $comments = Comment::query()->where('question_id',$questionId)->get();
+        return view('question/detail', compact('question','comments'));
     }
 
     /**
