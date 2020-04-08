714da7c7199b12632a4420c8e760959c5e179690
YaCpotato <yasshisshy@gmail.com>
2020-04-08 15:18:34 +0900

    ホーム画面作成

---
+++ resources/views/question/index.blade.php
@@ -0,0 +1,25 @@
+@extends('question/layout')
+@section('content')
+<div class="container ops-main" id="app">
+<div class="row">
+  <div class="col-md-12">
+    <div style="margin:10px;"><a href="/question/create" class="btn btn-primary">新規作成</a></div>
+  </div>
+</div>
+<div class="row">
+  <div class="col-md-9 col-md-offset-1" style="margin-right:30px;">
+    <table class="table text-center">
+      <tr>
+        <th class="text-center">question</th>
+      </tr>
+      @foreach($questions as $question)
+      <tr>
+        <td>
+          <span>{{ $question->content }}</span>
+        </td>
+        </tr>
+      @endforeach
+    </table>
+  </div>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -22,20 +22,20 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/question', 'QuestionController@index')->name('question_index');
-Route::get('/question', 'QuestionController@create')->name('question_create')->middleware('auth');
-Route::get('/question', 'QuestionController@store')->name('question_store')->middleware('auth');
-Route::get('/question', 'QuestionController@show')->name('question_show');
-Route::get('/question', 'QuestionController@edit')->name('question_edit')->middleware('auth');
-Route::get('/question', 'QuestionController@update')->name('question_update')->middleware('auth');
-Route::get('/question', 'QuestionController@destroy')->name('question_destroy')->middleware('auth');
+Route::get('/question_create', 'QuestionController@create')->name('question_create')->middleware('auth');
+Route::post('/question_store', 'QuestionController@store')->name('question_store')->middleware('auth');
+Route::get('/question_detail', 'QuestionController@show')->name('question_show');
+Route::get('/question_edit', 'QuestionController@edit')->name('question_edit')->middleware('auth');
+Route::put('/question_update', 'QuestionController@update')->name('question_update')->middleware('auth');
+Route::delete('/question_destroy', 'QuestionController@destroy')->name('question_destroy')->middleware('auth');
 
-Route::get('/answer', 'AnswerController@create')->name('answer_create')->middleware('auth');
-Route::get('/answer', 'AnswerController@store')->name('answer_store')->middleware('auth');
-Route::get('/answer', 'AnswerController@edit')->name('answer_edit')->middleware('auth');
-Route::get('/answer', 'AnswerController@update')->name('answer_update')->middleware('auth');
-Route::get('/answer', 'AnswerController@destroy')->name('answer_destroy')->middleware('auth');
+Route::get('/answer_create', 'AnswerController@create')->name('answer_create')->middleware('auth');
+Route::post('/answer_store', 'AnswerController@store')->name('answer_store')->middleware('auth');
+Route::get('/answer_edit', 'AnswerController@edit')->name('answer_edit')->middleware('auth');
+Route::put('/answer_update', 'AnswerController@update')->name('answer_update')->middleware('auth');
+Route::delete('/answer_destroy', 'AnswerController@destroy')->name('answer_destroy')->middleware('auth');
 
-Route::get('/comment', 'CommentController@index')->name('comment_index');
-Route::get('/comment', 'CommentController@store')->name('comment_store')->middleware('auth');
-Route::get('/comment', 'CommentController@update')->name('comment_update')->middleware('auth');
-Route::get('/comment', 'CommentController@destroy')->name('comment_destroy')->middleware('auth');
+// Route::get('/comment', 'CommentController@index')->name('comment_index');
+// Route::get('/comment', 'CommentController@store')->name('comment_store')->middleware('auth');
+// Route::get('/comment', 'CommentController@update')->name('comment_update')->middleware('auth');
+// Route::get('/comment', 'CommentController@destroy')->name('comment_destroy')->middleware('auth');
