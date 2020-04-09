d789680b78216cc336ba3e9d6a4a2a470be59b79
YaCpotato <yasshisshy@gmail.com>
2020-04-09 11:01:18 +0900

    微修正

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -19,7 +19,7 @@
                             <form method="POST" action="{{ route('password.email') }}">
                                 @csrf
 
-                                <div class="form-group row">
+                                <div class="form-group row" style="margin:8px;">
                                     <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
                                     <div class="col-md-6">
@@ -38,7 +38,7 @@
                                     </div>
                                 </div>
 
-                                <div class="form-group row mb-0">
+                                <div class="form-group row">
                                     <div class="col-md-6 offset-md-4">
                                         <button type="submit" class="button is-primary">
                                             {{ __('Send Password Reset Link') }}
