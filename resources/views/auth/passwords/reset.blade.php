737d1e706f6e7140370413b14318557e6d2fdaa5
YaCpotato <yasshisshy@gmail.com>
2020-04-09 10:27:20 +0900

    パスワードリセットのフォームCSS修正

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,10 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
+<section class="hero is-primary is-fullheight">
+  <div class="hero-body">
+    <div class="container">
+      <div class="columns is-centered">
+        <div class="column is-5-tablet is-4-desktop is-3-widescreen">
+            <div class="card auth-card">
                 <div class="card-header">{{ __('Reset Password') }}</div>
 
                 <div class="card-body">
@@ -17,8 +19,12 @@
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ $email ?? old('email') }}" required autocomplete="email" autofocus>
-
+                                <p class="control has-icons-left has-icons-right">
+                                    <input class="input" type="email" placeholder="Email" class="input is-primary @error('email') is-invalid @enderror" name="email" value="{{ $email ?? old('email') }}" required autocomplete="email" autofocus>
+                                    <span class="icon is-small is-left">
+                                    <i class="fas fa-envelope"></i>
+                                    </span>
+                                </p>
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -31,7 +37,12 @@
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+                                <p class="control has-icons-left has-icons-right">
+                                    <input id="password" class="input is-primary @error('password') is-invalid @enderror" type="password" placeholder="Password" name="password" required autocomplete="new-password">
+                                    <span class="icon is-small is-left">
+                                    <i class="fas fa-key"></i>
+                                    </span>
+                                </p>
 
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
@@ -45,13 +56,18 @@
                             <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
 
                             <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                                <p class="control has-icons-left has-icons-right">
+                                    <input id="password-confirm" class="input is-primary @error('password') is-invalid @enderror" type="password" placeholder="Password" name="password_confirmation" required autocomplete="new-password">
+                                    <span class="icon is-small is-left">
+                                    <i class="fas fa-key"></i>
+                                    </span>
+                                </p>
                             </div>
                         </div>
 
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="button is-primary">
                                     {{ __('Reset Password') }}
                                 </button>
                             </div>
