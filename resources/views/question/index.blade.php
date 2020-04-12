7797a3d41e2c4beddc86763c7ef7a7d6abac1b78
YaCpotato <yasshisshy@gmail.com>
2020-04-12 20:29:34 +0900

    メッセージ、出題ボタン追加

--- resources/views/question/index.blade.php
+++ resources/views/question/index.blade.php
@@ -4,6 +4,12 @@
     <div class="columns">
         <div class="column"><!--TBW--></div>
         <div class="column is-half">
+            <div class="intro-and-button-area box has-text-centered">
+                <p>
+                    RegExpノックセンターへようこそ<br>正規表現に関する日常生活での質問を投げたり、問題として出したり、<br>腕試しで挑戦したりできるよ！
+                </p>
+                <button class="button is-primary" style="margin: 8px;">問題を出す</button>
+            </div>
             <div class="trend">
             @foreach($questions as $question)
             <div class="card">
@@ -18,12 +24,7 @@
                 <footer class="card-footer">
                     <p class="card-footer-item">
                     <span>
-                        View on <a href="https://twitter.com/codinghorror/status/506010907021828096">Twitter</a>
-                    </span>
-                    </p>
-                    <p class="card-footer-item">
-                    <span>
-                        Share on <a href="#">Facebook</a>
+                        {{ $question->created_at }}
                     </span>
                     </p>
                 </footer>
