13888df3ed2735b011342e30337d79fa3d033d03
YaCpotato <yasshisshy@gmail.com>
2020-04-12 21:12:05 +0900

    個別ページレイアウトが完成

--- resources/views/question/detail.blade.php
+++ resources/views/question/detail.blade.php
@@ -1,14 +1,23 @@
 @extends('layouts.app')
 @section('content')
 <div class="container">
-    <div class="card indivi-card">
-        <div class="card-content">
+    <div class="card">
+        <div class="card-content detail-card-content">
             <p class="title">
                 {{ $question->content }}
             </p>
             <p class="subtitle">
             出題者：{{ $question->user->name }}
             </p>
+            <div class="example-area">
+                <p class="subtitle">例題と解答</p>
+                <p class="example-string">
+                    example string
+                </p>
+                <p class="highlight-string">
+                    example s<strong>tr</strong>ing
+                </p>
+            </div>
         </div>
         <footer class="card-footer">
             <p class="card-footer-item">
