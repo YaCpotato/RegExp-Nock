0fd4b880e1e2801853efb3aa3315a28045783ea6
YaCpotato <yasshisshy@gmail.com>
2020-04-13 22:07:09 +0900

    view_count追加

--- database/migrations/2020_04_13_130346_add_view_count_to_questions_table.php
+++ database/migrations/2020_04_13_130346_add_view_count_to_questions_table.php
@@ -14,7 +14,7 @@ class AddViewCountToQuestionsTable extends Migration
     public function up()
     {
         Schema::table('questions', function (Blueprint $table) {
-            //
+            $table->Integer('view_count')->default(0);
         });
     }
 
@@ -26,7 +26,7 @@ class AddViewCountToQuestionsTable extends Migration
     public function down()
     {
         Schema::table('questions', function (Blueprint $table) {
-            //
+            $table->dropColumn('view_count');
         });
     }
 }
