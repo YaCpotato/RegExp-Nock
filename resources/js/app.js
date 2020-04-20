356e9dbd9798b205fd7a06242ff2d0190ba16264
YaCpotato <yasshisshy@gmail.com>
2020-04-20 23:34:48 +0900

    不要コメント削除

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,26 +7,5 @@ const app = new Vue({
     el: '#app'
 });
 
-// $('#btnsend').on('click', function(){
-//     console.log('click')
-//     $('#result').text('通信中...');
-//     // Ajax通信を開始
-//     $.ajax({
-//         url:'http://localhost:8000/highlight',
-//         headers: {
-//             　　　'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-//             　　},
-//             type:'POST',
-//             data:{
-//                 base_string: '123-0099 ririeooo',
-//                 reg_exp: '/[0-9]{3}-[0-9]{4}/',
-//             },
-//         dataType: 'text',
-//         // フォーム要素の内容をハッシュ形式に変換
-//         timeout: 5000,
-//         success: function (result){
-//             console.log(result)
-//             $('#result').text(result);
-//         }
-//     })
-// })
+// '123-0099 ririeooo',
+// '/[0-9]{3}-[0-9]{4}/',
