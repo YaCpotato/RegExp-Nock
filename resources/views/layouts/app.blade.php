03590630db7abb0caf7e1ebd02e9afdbb13e8b84
YaCpotato <yasshisshy@gmail.com>
2020-04-25 00:30:46 +0900

    ヘッダーのリンクを修正

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,7 +15,7 @@
 <div class="container">
     <nav class="navbar" role="navigation" aria-label="main navigation">
     <div class="navbar-brand">
-        <a class="navbar-item" href="#">
+        <a class="navbar-item" href="/">
             <h1 class="title">RegExpノックセンター</h1>
         </a>
 
