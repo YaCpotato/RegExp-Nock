7b12dfc1eeadb44a5f616e74c8d861b09872239e
YaCpotato <yasshisshy@gmail.com>
2020-05-03 20:17:24 +0900

    containerのwidthを修正

--- resources/views/question/index.blade.php
+++ resources/views/question/index.blade.php
@@ -1,15 +1,15 @@
 @extends('layouts.app')
 @section('content')
-<div class="container">
+<div class="container is-fluid">
+        <div class="intro-and-button-area box has-text-centered">
+            <p>
+                RegExpノックセンターへようこそ<br>正規表現に関する日常生活での質問を投げたり、問題として出したり、<br>腕試しで挑戦したりできるよ！
+            </p>
+            <a class="button is-primary" style="margin: 8px;" href="{{ url('question_create') }}">問題を出す</a>
+        </div>
     <div class="columns">
         <div class="column"><!--TBW--></div>
         <div class="column is-half">
-            <div class="intro-and-button-area box has-text-centered">
-                <p>
-                    RegExpノックセンターへようこそ<br>正規表現に関する日常生活での質問を投げたり、問題として出したり、<br>腕試しで挑戦したりできるよ！
-                </p>
-                <a class="button is-primary" style="margin: 8px;" href="{{ url('question_create') }}">問題を出す</a>
-            </div>
             <div class="trend">
             @foreach($questions as $question)
             <a href="{{ url('question_detail',$question->id) }}">
