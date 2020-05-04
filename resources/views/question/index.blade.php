58856ef7fc50ca8eb6eb2d003643a7bb125dcf9a
YaCpotato <yasshisshy@gmail.com>
2020-05-04 23:48:50 +0900

    検索窓を追加

--- resources/views/question/index.blade.php
+++ resources/views/question/index.blade.php
@@ -2,10 +2,29 @@
 @section('content')
 <div class="main-index">
         <div class="intro-and-button-area has-text-centered">
-            <p>
-                RegExpノックセンターへようこそ<br>正規表現に関する日常生活での質問を投げたり、問題として出したり、<br>腕試しで挑戦したりできるよ！
+            <p class="title">
+                RegExpノックセンターへようこそ
             </p>
-            <a class="button is-primary" style="margin: 8px;" href="{{ url('question_create') }}">問題を出す</a>
+            <div class="columns">
+            <div class="column">
+                <p>正規表現に関する日常生活での質問を投げたり、問題として出したり</p>
+                <a class="button is-primary" style="margin: 8px;" href="{{ url('question_create') }}">問題を出す</a>
+            </div>
+            <div class="is-divider-vertical" data-content="OR"></div>
+            <div class="column">
+                腕試しで挑戦したりできるよ！
+                <div class="field has-addons has-addons-centered">
+                    <div class="control">
+                        <input class="input" type="text" placeholder="問題を探す">
+                    </div>
+                        <div class="control">
+                            <a class="button is-info">
+                            Search
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     <div class="columns">
         <div class="column"><!--TBW--></div>
